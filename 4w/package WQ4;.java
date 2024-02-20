79ad1612708b949fe23a5cea6914a16f8eebe7ea
gyenug <155498461+gyenug@users.noreply.github.com>
2024-02-20 10:34:33 +0900

    Add files via upload

---
+++ 4w/package WQ4;.java
@@ -0,0 +1,33 @@
+package WQ4;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CorrectGualho {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+
+        System.out.println(solution(inputString));
+    }
+
+    static boolean solution(String s) {
+        boolean answer = true;
+        char[] charArray = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : charArray) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+        return answer;
+    }
+}
